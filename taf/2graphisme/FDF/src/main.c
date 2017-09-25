ddcf1297374ccb0fecacf0e352a0aa2017b1493b
Elouan ABGRALL <eabgrall@e1r2p15.42.fr>
2017-09-25 17:31:24 +0200

    fdf...

--- taf/2graphisme/FDF/src/main.c
+++ taf/2graphisme/FDF/src/main.c
@@ -6,15 +6,27 @@
 /*   By: eabgrall <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/21 15:26:30 by eabgrall          #+#    #+#             */
-/*   Updated: 2017/09/21 20:13:09 by eabgrall         ###   ########.fr       */
+/*   Updated: 2017/09/25 17:28:23 by eabgrall         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
+#include <stdio.h>
 
 int		on_key(int keycode, void *param)
 {
+	int		x;
+	int		y;
 
+	(void)param;
+	printf("event : %d\n", keycode);
+	if (keycode == 53)
+		exit(0);
+	if (keycode == 2)
+	{
+		while (x)
+	}
+	return (0);
 }
 
 int		main(int ac, char **av)
@@ -25,7 +37,7 @@ int		main(int ac, char **av)
 	int		y;
 
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, 400, 400, "fdf");
+	win = mlx_new_window(mlx, 900, 900, "fdf");
 	y = 50;
 	while (y < 350)
 	{
@@ -37,6 +49,7 @@ int		main(int ac, char **av)
 		}
 		y++;
 	}
+	mlx_key_hook(win, on_key, 0);
 	mlx_loop(mlx);
 	ac = 0;
 	av = 0;
